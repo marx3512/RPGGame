59d933678098bbea264a450a6e2e52a256148bd6
Marx <marxborgesmachado@gmail.com>
2021-08-31 16:49:53 -0300

    Teste new Script

---
+++ RPGGame/Assets/Testa.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testa : MonoBehaviour
+{
+    public Transform teste;
+    [SerializeField] private GameObject body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
